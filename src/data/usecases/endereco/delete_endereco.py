f4b14f8a2233e1322f0002be28315c5509d80879
cassi35 <cgrsobral@gmail.com>
2025-09-15 21:44:52 -0300

    fazendo usecase regra de negocio do delete

--- src/data/usecases/endereco/delete_endereco.py
+++ src/data/usecases/endereco/delete_endereco.py
@@ -1,9 +1,38 @@
 from src.domain.usecases.endereco.delete_endereco import EnderecoDeleteUseCase as EnderecoDeleteInterface
 from src.data.interfaces.endereco_interface_repository import EnderecoRepositoryInterface
+from src.errors.types.http_not_found import HttpNotFoundError
+from src.errors.types.http_bad_request import HttpBadRequestError
+from typing import Dict
 
 class EnderecoDeleteUseCase(EnderecoDeleteInterface):
     def __init__(self, endereco_repository: EnderecoRepositoryInterface):
         self.endereco_repository = endereco_repository
-    
-    def delete(self, endereco_id: int) -> None:
-        pass
+
+    def delete(self, endereco_id: int) -> Dict:
+        # Valida se o ID é válido
+        self._validate_endereco_id(endereco_id)
+        
+        # Verifica se o endereço existe
+        self._exists_endereco(endereco_id=endereco_id)
+        
+        # Deleta o endereço
+        self.endereco_repository.delete_endereco(endereco_id=endereco_id)
+        
+        # Retorna resposta de sucesso
+        return {
+            "type": "Endereco",
+            "count": 1,
+            "attributes": {
+                "id": endereco_id,
+                "deleted": True
+            }
+        }
+
+    def _validate_endereco_id(self, endereco_id: int) -> None:
+        if not isinstance(endereco_id, int) or endereco_id <= 0:
+            raise HttpBadRequestError("ID do endereco deve ser um numero positivo")
+
+    def _exists_endereco(self, endereco_id: int) -> None:
+        endereco = self.endereco_repository.select_endereco(endereco_id)  # Corrigido parâmetro
+        if not endereco:
+            raise HttpNotFoundError("endereco nao encontrado")
--- src/domain/usecases/endereco/delete_endereco.py
+++ src/domain/usecases/endereco/delete_endereco.py
@@ -2,6 +2,6 @@ from abc import ABC, abstractmethod
 
 class EnderecoDeleteUseCase(ABC):
     @abstractmethod
-    def delete(self, endereco_id: int) -> None:
+    def delete(self, endereco_id: int) -> dict:
         """Delete endereco by id usecase"""
         pass
